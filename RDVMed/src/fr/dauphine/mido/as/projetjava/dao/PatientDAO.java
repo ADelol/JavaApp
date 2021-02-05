93fe092f74965617b4bd20dae3341eba0b16aee1
ADelol <arnaud.delol@gmail.com>
2021-02-05 09:43:52 +0100

    Bug résolu, il met autocommit à true de base ... 			<property name="defaultAutoCommit" value="false"/> à ajouter dans persistence.xml

--- RDVMed/src/fr/dauphine/mido/as/projetjava/dao/PatientDAO.java
+++ RDVMed/src/fr/dauphine/mido/as/projetjava/dao/PatientDAO.java
@@ -1,39 +1,41 @@
 package fr.dauphine.mido.as.projetjava.dao;
 
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 
 import fr.dauphine.mido.as.projetjava.entityBeans.Patient;
 
+
 public class PatientDAO {
 
-	private EntityManagerFactory emf;
 
-	public PatientDAO() {
-		emf = Persistence.createEntityManagerFactory("RDVMed");
-	}
+	
+	public  void ajouterPatient(Patient p) {
 
-	public void ajouterPatient(Patient p) {
-		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("RDVMed");
+			Map<String,Object> s = emf.getProperties();
+			System.out.println(s);
 			EntityManager em = emf.createEntityManager();
 			EntityTransaction et = em.getTransaction();
-			em.getTransaction().begin();
+			et.begin();
 			em.persist(p);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			et.commit();
+
 	}
 
 	public Patient getPatient(String mail) {
 		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("RDVMed");
 			EntityManager em = emf.createEntityManager();
 			return em.find(Patient.class, mail);
-		} catch (Exception e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		return null;
